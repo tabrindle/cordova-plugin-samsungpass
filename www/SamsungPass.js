7eb862fa4ae5d33e06f4e108eb6b639e0c9cbe1e
Trevor Brindle <trevorb@marketamerica.com>
2015-05-19 15:15:59 -0400

    Added JS checkSamsungPassSupport function

--- www/SamsungPass.js
+++ www/SamsungPass.js
@@ -10,5 +10,15 @@ SamsungPass.prototype.GenericFailureCallback = function(response) {
     console.log(response);
 };
 
+SamsungPass.prototype.checkSamsungPassSupport = function(SuccessCallback, FailureCallback) {
+    console.log('checkSamsungPassSupport');
+
+    if (SuccessCallback && FailureCallback) {
+        cordova.exec(SuccessCallback, FailureCallback, 'SamsungPassPlugin', 'checkSamsungPassSupport', []);
+    } else {
+        cordova.exec(SamsungPass.GenericSuccessCallback, SamsungPass.GenericFailureCallback, 'SamsungPassPlugin', 'checkSamsungPassSupport', []);
+    }
+};
+
 var SamsungPass = new SamsungPass();
 module.exports = SamsungPass;
