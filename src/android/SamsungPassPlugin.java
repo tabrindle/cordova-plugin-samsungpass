5c18a0529f4952af216f7c1f669bfb9f6c2e2f28
Trevor Brindle <trevorb@marketamerica.com>
2015-05-19 14:13:57 -0400

    Import Cordova to Plugin

--- src/android/SamsungPassPlugin.java
+++ src/android/SamsungPassPlugin.java
@@ -1,5 +1,8 @@
 package com.cordova.plugin.samsung.pass;
 
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
 import com.samsung.android.sdk.SsdkUnsupportedException;
 import com.samsung.android.sdk.pass.Spass;
 import com.samsung.android.sdk.pass.SpassFingerprint;
